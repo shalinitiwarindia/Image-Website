ba181a56dc9a26d3e77ac4b363d58d332e6f8bc5
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-08-21 17:13:55 +0530

    Create navbar.js

---
+++ components/navbar.js
@@ -0,0 +1,17 @@
+let navbar = ()=>{
+    return `<div id="search_bar">
+            <input type="text" id="query" placeholder="Enter text here for search" />
+            <div id="categories">
+            <h3 id="nature">Nature</h3>
+            <h3 id="car">Cars</h3>
+            <h3 id="music">Music</h3>
+            <h3 id="cartoon">Cartoons</h3>
+            <h3 id="technology">Technology</h3>
+            <h3 id="marvel">Marvel</h3>
+
+        </div>
+        </div>
+        `;
+};
+
+export {navbar};
