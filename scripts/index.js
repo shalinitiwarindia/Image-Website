184d5ccfe93e00dae21cca4a7114dfd2e811a96b
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-08-22 21:20:41 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,3 @@
-const API="your api key";
 
-import { navbar } from "../components/navbar.js";
-let n=document.getElementById("navbar");
-n.innerHTML=navbar();
-
-import { searchImages,append } from "./fetch.js";
-
-
-};
-
-document.getElementById("query").addEventListener("keydown",search);
-;
-}
-
-
-
-//     }
-// }
 
 
